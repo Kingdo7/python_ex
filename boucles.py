2cd3a29d3d4fc101503a88eb6c829a264db96551
Julie <angela.j@laposte.net>
2025-10-06 15:32:06 +0200

    boucles ex 2

--- boucles.py
+++ boucles.py
@@ -1,9 +1,13 @@
-#Exercice
-
-#Écrivez un programme qui affiche les 20 premiers termes de la table de multiplication par 7, en signalant au passage 
-#(à l'aide d'une astérisque) ceux qui sont des multiples de 3.
-#Exemple : 7 14 21 * 28 35 42 * 49 56 63 * 70 77 84 * 91 98 105 * 112 119 126 * 133 140
-
+# https://python.developpez.com/exercices
+##############
+# Exercice 1 #
+##############
+"""
+Écrivez un programme qui affiche les 20 premiers termes de la table de multiplication par 7, en signalant au passage 
+(à l'aide d'une astérisque) ceux qui sont des multiples de 3.
+Exemple : 7 14 21 * 28 35 42 * 49 56 63 * 70 77 84 * 91 98 105 * 112 119 126 * 133 140
+"""
+"""
 nb = 0
 
 while nb <= 7*20:
@@ -13,3 +17,24 @@ while nb <= 7*20:
         else:
             print(nb)
     nb += 1
+"""
+##############
+# Exercice 2 #
+##############
+
+"""
+Écrivez un programme qui affiche une table de conversion de sommes d'argent exprimées en euros, en dollars canadiens. La progression des sommes de la table sera « géométrique », comme dans l'exemple ci-dessous :
+1 euro(s) = 1.65 dollar(s)
+2 euro(s) = 3.30 dollar(s)
+4 euro(s) = 6.60 dollar(s)
+8 euro(s) = 13.20 dollar(s)
+etc. (S'arrêter à 16384 euros)
+"""
+
+euro = 1
+taux = 1.65
+
+while euro <= 16384:
+    dollar = taux * euro
+    print(str(euro) + " euro(s) = " + str(dollar) + " dollar(s)")
+    euro *= 2
